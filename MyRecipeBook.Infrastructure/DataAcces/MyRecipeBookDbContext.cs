8b8608b930ba987ae415de54bfe0bd367ebb85e8
MurilloKelvin <murillokelvin.a@gmail.com>
2025-08-10 10:54:28 -0300

    feat: adding pass cryptography and read and write database functions

---
+++ MyRecipeBook.Infrastructure/DataAcces/MyRecipeBookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAcces;
+
+public class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions options) : base(options) { } // construtor que recebe as opções de configuração do DbContext
+    
+    public DbSet<User> Users { get; set; } // existe uma tabela Users no banco de dados
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyRecipeBookDbContext).Assembly);
+    } 
+}
---
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Application.Services.Cryptography;
+
+public class PasswordEncripter
+{
+    // func para encriptar a senha usando SHA-512
+    public string Encrypt(string password)
+    {
+        var salt = "JQW!@";  // string aleatoria
+        
+        var newPass = $"{password}{salt}"; // adiciona um salt para aumentar a segurança da senha
+        
+        var bytes = Encoding.UTF8.GetBytes(newPass);
+        var hashBytes = SHA512.HashData(bytes);
+        
+        return StringBytes(hashBytes);
+    }
+
+    public static string StringBytes(byte[] bytes) // converte array de bytes para string hexadecimal
+    {
+        var sb = new StringBuilder();
+        foreach (var b in bytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+        return sb.ToString();
+    }
+}
